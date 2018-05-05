e186ee9133c71f15bb948fcd30a579ab04e2a50d
hopperhuang <huanjiwe@126.com>
2018-05-06 02:11:53 +0800

    1.Feature: combine dva with next.js

--- pages/index.js
+++ pages/index.js
@@ -2,56 +2,35 @@
 import Link from 'next/link';
 import React from 'react';
 import dva, { connect } from 'dva';
+// import dva from 'dva';
 
-
-// class Test extends React.Component {
-//   render() {
-//     return (<div>2333</div>);
-//   }
-// }
+const page = (props) => {
+  const { index } = props;
+  const { name } = index;
+  return (
+    <div>
+      Hi,{name}!! &nbsp;
+      <Link href="/users">Go to /users</Link>
+    </div>
+  );
+};
 
 const model = {
   namespace: 'index',
   state: {
-    index: true,
-  },
-  subscriptions: {
-    setup({ dispatch }) {
-      // console.log(dispatch);
-    },
+    name: 'hopperhuang',
   },
 };
-
-const App = (props) => {
-  console.log(props);
-  return (
-    <div>
-      Hi,
-      <Link href="/users">
-        <a>
-           Go to /users
-        </a>
-      </Link>
-    </div>
-  );
-};
-const app = dva();
-app.model(model);
-const Page = connect()(App);
-app.router(Page);
-const Component = app.start();
-// console.log(Component);
-
-// export default Component;
-
 
 export default function () {
-  // const app = dva();
-  // app.model(model);
-  // app.router(App);
+  const app = dva();
+  const ConnectedComponent = connect((state) => { return { index: state.index }; })(page);
+  const main = () => {
+    return <ConnectedComponent />;
+  };
+  app.model(model);
+  app.router(main);
 
-  // const Component = app.start();
-  return (
-    <Component />
-  );
+  const Component = app.start();
+  return <Component />;
 }
