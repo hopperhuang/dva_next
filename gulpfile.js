00d1867d79de5f75138b03b3704364569ffa8a57
hopperhuang <huanjiwe@126.com>
2018-05-12 18:41:22 +0800

    1.Feature: fommat files

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var pxtorem = require('postcss-pxtorem');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const pxtorem = require('postcss-pxtorem');
 
-gulp.task('css', function () {
-    var processors = [
-        autoprefixer({
-            browsers: [
-                "> 1%",
-                "last 2 versions"
-            ]
-        }),
-        pxtorem({
-            "rootValue": 100,
-            "unitPrecision": 5,
-            "propList": [
-              "*"
-            ],
-            "selectorBlackList": [],
-            "replace": false,
-            "mediaQuery": false,
-            "minPixelValue": 2
-        })
-    ];
-    return gulp.src(['node_modules/antd-mobile/dist/antd-mobile.css'])
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./static/stylesheet'));
+gulp.task('css', () => {
+  const processors = [
+    autoprefixer({
+      browsers: [
+        '> 1%',
+        'last 2 versions',
+      ],
+    }),
+    pxtorem({
+      rootValue: 100,
+      unitPrecision: 5,
+      propList: [
+        '*',
+      ],
+      selectorBlackList: [],
+      replace: false,
+      mediaQuery: false,
+      minPixelValue: 2,
+    }),
+  ];
+  return gulp.src(['node_modules/antd-mobile/dist/antd-mobile.css'])
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./static/stylesheet'));
 });
 gulp.task('default', ['css']);
