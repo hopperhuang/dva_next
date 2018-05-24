4c1088bbe7aa1ae9990bde21302610c70b7cf096
hopperhuang <huanjiwe@126.com>
2018-05-24 23:09:51 +0800

    1.Feature: check isServer or not to show toast

--- model/checkRequestResult.js
+++ model/checkRequestResult.js
@@ -2,9 +2,10 @@ import { put } from 'redux-saga/effects';
 import { Toast } from 'antd-mobile';
 import auth from '../utils/auth';
 
-// const { put } = sagaEffects;
+const checkServer = () => Object.prototype.toString.call(global.process) === '[object process]';
 
 function *checkResquetResult(response) {
+  const isServer = checkServer();
   const { data } = response;
   if (data) {
     yield put({ type: 'login/changeStateToLogin' });
@@ -12,11 +13,15 @@ function *checkResquetResult(response) {
   } else {
     const { code } = response.err;
     if (code) { // 参数错误
-      Toast.info('登陆失败', 2);
+      if (!isServer) {
+        Toast.info('登陆失败', 2);
+      }
       yield put({ type: 'login/changeStateToUnlogin' });
       auth.removeTokenFromCookie();
     } else { // 网络错误
-      Toast.info('网络错误');
+      if (!isServer) {
+        Toast.info('网络错误');
+      }
       yield put({ type: 'login/changeStateToUnlogin' });
     }
   }
